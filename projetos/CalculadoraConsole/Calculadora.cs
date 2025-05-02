09054000cb9c7e4b000444cb60fc6056a9803c6e
IsraelBonicenha <israelbonicenha2020@gmail.com>
2025-05-02 11:02:55 -0300

    feat: adiciona classe Calculadora com operações matemáticas básicas

---
+++ projetos/CalculadoraConsole/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculadoraConsole
+{
+    class Calculadora
+    {
+        public static float Somar(float num1, float num2) => num1 + num2;
+        public static float Subtrair(float num1, float num2) => num1 - num2;
+        public static float Multiplicar(float num1, float num2) => num1 * num2;
+        public static float Dividir(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                ConsoleMsg.Red("Não é possível dividir por zero.");
+                return 0;
+            }
+            return num1 / num2;
+        }
+    }
+}
