c469279c861e87ac32182d2453f8ecb54e7fb71c
IsraelBonicenha <israelbonicenha2020@gmail.com>
2025-05-02 10:36:53 -0300

    feat: adiciona classe ConsoleMsg com métodos para exibir mensagens coloridas no console

---
+++ projetos/CalculadoraConsole/ConsoleMsg.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Mesmo não utilizando todos os métodos dessa classe, achei válido adicionar para aprendizado.
+    Penso que, na pior das hipóteses, pode ser útil para um projeto futuro rsrs.
+ */
+
+namespace CalculadoraConsole
+{
+    public static class ConsoleMsg
+    {
+        public static void Blue(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Red(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Yellow(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Green(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+
+        public static void Print(string msg, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
